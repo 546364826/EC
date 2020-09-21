af145cd350e77e4cab12b36c403730cb5ac7c779
546364826 <48347103+546364826@users.noreply.github.com>
2020-09-21 14:11:39 +0800

    Update SmsListener.java

--- Project_parent/Project_sms/src/main/java/com/zwj/listener/SmsListener.java
+++ Project_parent/Project_sms/src/main/java/com/zwj/listener/SmsListener.java
@@ -39,8 +39,8 @@ public class SmsListener {
             final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
             final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
                 //替换成你的AK
-            final String accessKeyId = "LTAIEvcYuqq9ow5z";//你的accessKeyId,参考本文档步骤2
-            final String accessKeySecret = "PyhniMfzqt6r2RMVxuB1xS3BN1e2rW";//你的accessKeySecret，参考本文档步骤2
+            final String accessKeyId = "";//你的accessKeyId,参考本文档步骤2
+            final String accessKeySecret = "";//你的accessKeySecret，参考本文档步骤2
                 //初始化ascClient,暂时不支持多region（请勿修改）
             IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 
